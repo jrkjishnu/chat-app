496c9b5c100fd88e0a81e457a7fe7674e3690b82
jrkjishnu <jishnurajk4@gmail.com>
2021-09-27 11:54:52 +0530

    Storing users finished

---
+++ src/utils/users.js
@@ -0,0 +1,76 @@
+const users = []
+
+//add users, remove users, getusers, get users in room
+
+const addUser = ({ id, username, room })=>{
+    //Clean the data
+    username = username.trim().toLowerCase()
+    room = room.trim().toLowerCase()
+
+    //validate the data
+    if(!username || !room){
+        return {
+            error:'Username and room are rquired!'
+        }
+    }
+
+    //check for existing user
+    const existingUser = users.find((user) =>{
+        return user.room === room && user.username === username
+    })
+
+    //Validate username
+    if(existingUser){
+        return {
+            error:'Username is in use!'
+        }
+    }
+
+    //Store user
+
+    const user = { id, username, room}
+    users.push(user)
+    return {user}
+}
+
+
+const removeUser = (id)=>{
+    const index = users.findIndex((user)=>{
+        return user.id === id
+    })
+
+    if(index !== -1)
+    {
+        return users.splice(index,1)[0]
+    }
+}
+
+const getUser = (id)=>
+{
+    return users.find((user)=>user.id === id) 
+}
+
+const getUsersInRoom = (room)=>
+{
+    const user = users.filter((user)=>user.room === room)
+    return user;
+}
+
+addUser({
+    id:10,
+    username:'jk',
+    room:'chunda'
+})
+addUser({
+    id:22,
+    username:'jkkkk',
+    room:'pul'
+})
+addUser({
+    id:50,
+    username:'jrrrk',
+    room:'chunda'
+})
+
+console.log(getUsersInRoom('chuna'));
+//console.log(getUser(50));
